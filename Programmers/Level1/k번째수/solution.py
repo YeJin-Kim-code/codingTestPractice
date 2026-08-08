5e1330d7cf3bf0b397c30d9180987ccd8768bdb1
YeJin-Kim-code <109929675+YeJin-Kim-code@users.noreply.github.com>
2026-08-08 10:20:00 +0900

    Create solution.py

---
+++ Programmers/Level1/k번째수/solution.py
@@ -0,0 +1,8 @@
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        i, j, k = command
+        sliced_array = array[i-1:j]
+        sliced_array.sort()
+        answer.append(sliced_array[k-1])
+    return answer
